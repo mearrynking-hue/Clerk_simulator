6c3e42e08ffa981282eee8084a0a466c9ae88294
Polina <mearrynking@gmail.com>
2026-05-11 17:02:43 +0200

    Added class Complaint

--- bureaucracy.py
+++ bureaucracy.py
@@ -22,3 +22,14 @@ class Document(ABC):
         def __str__(self):
             return f"[{self.__class__.__name__}] Subject: '{self.subject} | Submitter: {self.submitter} | Status: {self.status.value}"
         
+class Complaint(Document):
+    def __init__(self, submitter, subject, complainant=True, **kwargs):
+        super().__init__(submitter, subject, **kwargs)
+        self.complainant = complainant
+
+    def verify(self):
+        if self.complainant:
+            self.status = Status.APPROVED
+        else:
+            self.status = Status.REJECTED
+            self.notes = "Complainant must be specified"
