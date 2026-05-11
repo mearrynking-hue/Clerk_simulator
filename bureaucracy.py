afa03adb81b0dd1f4e83930c53d058f0ee2ca395
Polina <mearrynking@gmail.com>
2026-05-11 16:33:17 +0200

    Added class Document

--- bureaucracy.py
+++ bureaucracy.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from enum import Enum
 
 class Status(Enum):
@@ -5,3 +6,19 @@ class Status(Enum):
     APPROVED = "Approved"
     REJECTED = "Rejected"
     REVISION = "Revision"
+
+class Document(ABC):
+    def __init__(self, submitter, subject, signed=None, notes=""):
+        self.submitter = submitter
+        self.subject = subject
+        self.signed = signed
+        self.notes = notes
+        self.status = Status.PENDING
+
+        @abstractmethod
+        def verify(self):
+            pass
+
+        def __str__(self):
+            return f"[{self.__class__.__name__}] Subject: '{self.subject} | Submitter: {self.submitter} | Status: {self.status.value}"
+        
